77bc2f344cad5ae44d3101f005a2cd6f9ba38a15
Priyanshu suryawanshi <132843870+priyanshu1344@users.noreply.github.com>
2025-03-24 15:09:04 +0530

    Update Replace Multiple Spaces with a Single Space.py

--- Assignment-5-22-03-2025/Replace Multiple Spaces with a Single Space.py
+++ Assignment-5-22-03-2025/Replace Multiple Spaces with a Single Space.py
@@ -5,3 +5,9 @@ def normalize_spaces(text):
 
 text = input("Enter a sentence: ")
 print("Cleaned text:", normalize_spaces(text))
+
+
+
+# print(normalize_spaces("This   sentence   has    too   many spaces."))  # Expected Output: "This sentence has too many spaces."
+# print(normalize_spaces("No extra spaces here."))  # Expected Output: "No extra spaces here."
+# print(normalize_spaces("     Leading and trailing spaces should be removed too.    "))  # Expected Output: "Leading and trailing spaces should be removed too."
