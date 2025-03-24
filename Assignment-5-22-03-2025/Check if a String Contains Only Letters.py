c3ffb86bda437caa020c4ff42c6a3e2b8193853b
Priyanshu suryawanshi <132843870+priyanshu1344@users.noreply.github.com>
2025-03-24 15:07:13 +0530

    Update Check if a String Contains Only Letters.py

--- Assignment-5-22-03-2025/Check if a String Contains Only Letters.py
+++ Assignment-5-22-03-2025/Check if a String Contains Only Letters.py
@@ -5,3 +5,9 @@ def is_only_letters(text):
 
 text = input("Enter a string: ")
 print("true" if is_only_letters(text) else "False")
+
+
+
+# print(is_only_letters("HelloWorld"))  # Expected Output: True
+# print(is_only_letters("Hello123"))    # Expected Output: False
+# print(is_only_letters("Python!"))     # Expected Output: False
