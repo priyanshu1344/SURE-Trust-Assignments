85c260723b73ebc8d37ea1955b54089297109cff
Priyanshu suryawanshi <132843870+priyanshu1344@users.noreply.github.com>
2025-03-24 15:06:13 +0530

    Update Validate Email Addresses.py

--- Assignment-5-22-03-2025/Validate Email Addresses.py
+++ Assignment-5-22-03-2025/Validate Email Addresses.py
@@ -10,3 +10,9 @@ if is_valid_email(email):
     print("Valid email")
 else:
     print("Invalid email")
+
+# print(is_valid_email("student@example.com"))  # Expected Output: True
+# print(is_valid_email("user.name@my-site.org"))  # Expected Output: True
+# print(is_valid_email("invalid-email.com"))  # Expected Output: False
+# print(is_valid_email("missing@domain"))  # Expected Output: False
+# print(is_valid_email("@nouser.com"))  # Expected Output: False
