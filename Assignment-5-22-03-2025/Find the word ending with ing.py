ca5b8a4cd1033a9366301d90357864822bc13142
Priyanshu suryawanshi <132843870+priyanshu1344@users.noreply.github.com>
2025-03-24 15:08:13 +0530

    Update Find the word ending with ing.py

--- Assignment-5-22-03-2025/Find the word ending with ing.py
+++ Assignment-5-22-03-2025/Find the word ending with ing.py
@@ -5,3 +5,10 @@ def find_ing_words(text):
 
 text = input("Enter a sentence: ")
 print("Words ending with 'ing':", find_ing_words(text))
+
+
+
+
+# print(find_ing_words("I am running and she is singing."))  # Expected Output: ['running', 'singing']
+# print(find_ing_words("Walking is fun but jumping is better!"))  # Expected Output: ['Walking', 'jumping']
+# print(find_ing_words("No ING words here!"))  # Expected Output: []
