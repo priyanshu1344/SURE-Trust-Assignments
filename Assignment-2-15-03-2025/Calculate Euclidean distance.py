0e485c9b839d2b2cff85dfee6b9675d8a83714ac
Priyanshu suryawanshi <132843870+priyanshu1344@users.noreply.github.com>
2025-05-19 21:22:10 +0530

    Update Calculate Euclidean distance.py

--- Assignment-2-15-03-2025/Calculate Euclidean distance.py
+++ Assignment-2-15-03-2025/Calculate Euclidean distance.py
@@ -2,7 +2,7 @@
 #Write a Python program to calculate Euclidean Distance. 
 
 
-def euclidean_distance(x1, y1, x2, y2):
+def euclidean_distance(x1, y1, x2, y2): 
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 x1, y1 = map(float, input("Enter first point (x1 y1): ").split())
